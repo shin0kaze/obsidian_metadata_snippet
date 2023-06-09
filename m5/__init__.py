98f1f7ec6886ee5e938d37b96d5bbf1937b28b47
usr <kirill.moskva@mail.ru>
2023-06-09 17:02:54 +0600

    fix to ruamel

--- m5/__init__.py
+++ m5/__init__.py
@@ -1,4 +1,6 @@
-from yaml import safe_load, safe_dump
+import ruamel.yaml
+from io import StringIO
+yaml = ruamel.yaml.YAML()
 
 
 def r(vpath, npath):
@@ -19,17 +21,18 @@ def r(vpath, npath):
             # k, v = l[:-1].split(': ')
             # d[k] = v[1:-1]
     chunk = "".join(lst)
-    yaml = safe_load(chunk)
-    return yaml if yaml is not None else {}
+    dct = yaml.load(chunk)
+    return dct if dct is not None else {}
 
 
-def w(vpath, npath, d):
-    orig_d = r(vpath, npath)
-    # merge two dics, d has will overwrite
-    # new_d = orig_d | d
-    new_meta = safe_dump(d)
+def w(vpath, npath, dct):
+    orig_dct = r(vpath, npath)
+    sio = StringIO()
+    yaml.dump(dct, sio)
+    new_meta = sio.getvalue()
+    sio.close()
     fpath = f'{vpath}/{npath}'
-    until_line = len(orig_d)
+    until_line = line_count(orig_dct)
     with open(fpath, 'r', encoding='utf-8') as fi:
         with open(fpath, 'r+', encoding='utf-8') as fo:
             if not is_meta_exists():
@@ -39,12 +42,12 @@ def w(vpath, npath, d):
             fi.readline()
             seek_pos = fi.tell()
             fo.seek(seek_pos, 0)
-            # skip not needed lines
+            # skip old meta
             while cur_lineno <= until_line:
                 cur_lineno += 1
                 fi.readline()
 
-            # insert our text
+            # insert our new meta
             fo.writelines(new_meta)
 
             # insert rest of the file
@@ -55,6 +58,18 @@ def w(vpath, npath, d):
             fo.truncate()
 
 
+def line_count(dct):
+    count = 0
+    for v in list(dct.values()):
+
+        # check multiline
+        ml_count = v.count('\n')
+        ml_count = ml_count + 1 if ml_count > 0 else 0
+
+        count += 1 + ml_count
+    return count
+
+
 def is_meta_exists():
     return True
 
@@ -63,7 +78,7 @@ def add_empty_meta():
     pass
 
 
-if __name__ == '__main__' and False:
+if __name__ == '__main__' and True:
     v = r('.', 'test.txt')
     print(v)
 
@@ -71,5 +86,5 @@ if __name__ == '__main__' and False:
     v['d'] = 'bbb'
     del v['a']
     w('.', 'test2.txt', v)
-    v2 = r('.', 'test.txt')
-    print(v)
+    v2 = r('.', 'test2.txt')
+    print(v2)
